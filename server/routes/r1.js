f9584b784c3d844f151aeb02f50eac9fa0bfdbe5
sahil <sahilsinghssr1487@gmail.com>
2024-07-11 17:44:12 +0530

    ab mei aur ni krra hogya final yahi samjhle

--- server/routes/r1.js
+++ server/routes/r1.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const app = express();
 const router= express.Router();
-const { signup , makeAppointment , login, checkLoginStatus, test, gettest, jwtDecode}=require('../controllers/handler');
+const { signup , makeAppointment , login, checkLoginStatus, test, gettest, jwtDecode,showAppointments}=require('../controllers/handler');
 router.post('/signup', signup);
-router.post("/login", login)
-router.post("/checkLoginStatus", checkLoginStatus)
-router.post("/test", jwtDecode, test)
+router.post("/login", login);
+router.post("/showAppointments", showAppointments);
+router.post("/checkLoginStatus", checkLoginStatus);
+router.post("/test", jwtDecode, test);
 router.post("/gettest", gettest)
 router.post("/makeappointment", jwtDecode, makeAppointment)
 module.exports = router;
